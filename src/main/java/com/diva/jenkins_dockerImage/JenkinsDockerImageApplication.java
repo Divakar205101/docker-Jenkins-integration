677bd76a6787aafa4619de248445f99d62041b63
Divakar205101 <sdivakar342@gmail.com>
2024-12-14 16:34:34 +0530

    first commit

---
+++ src/main/java/com/diva/jenkins_dockerImage/JenkinsDockerImageApplication.java
@@ -0,0 +1,13 @@
+package com.diva.jenkins_dockerImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diva/jenkins_dockerImage/JenkinsDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diva.jenkins_dockerImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
